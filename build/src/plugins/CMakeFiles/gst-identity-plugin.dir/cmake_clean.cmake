f126907ddec7ada06a9ae337600e869c2d05c7bc
Kaunil Dhruv <dhruv.kaunil@gmail.com>
2021-01-23 17:36:26 +0000

    added identity plugin

---
+++ build/src/plugins/CMakeFiles/gst-identity-plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gst-identity-plugin.dir/gstidentity.cpp.o"
+  "../../aarch64/lib/libgst-identity-plugin.pdb"
+  "../../aarch64/lib/libgst-identity-plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gst-identity-plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
